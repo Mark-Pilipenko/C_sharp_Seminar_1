12abe73b7f9726cde282a4e4d17ac87aafde38a7
Mark <kalmark@mail.ru>
2022-10-03 22:26:40 +0300

    Add Ex_1

---
+++ Ex_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Input first noun ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second noun ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b)
+{
+    Console.Write(a);
+    Console.Write(" = ");
+    Console.Write(b);
+    Console.WriteLine("^2");
+}
+else 
+{
+    Console.Write(a);
+    Console.Write(" <> ");
+    Console.Write(b);
+    Console.WriteLine("^2"); 
+}
