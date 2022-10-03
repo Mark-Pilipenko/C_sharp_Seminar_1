54220503b6864528cb2bc98c490a686220d941d3
Mark <kalmark@mail.ru>
2022-10-03 22:40:05 +0300

    Add Ex_2

---
+++ Ex_2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Input noun from 1 to 7 ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("First day of the week is Monday!");
+}
+if (a == 2)
+{
+    Console.WriteLine("Second day of the week is Tuesday!");
+}
+if (a == 3)
+{
+    Console.WriteLine("Third day of the week is Wednesday!");
+}
+if (a == 4)
+{
+    Console.WriteLine("Fourth day of the week is Thursday!");
+}
+if (a == 5)
+{
+    Console.WriteLine("Fifth day of the week is Friday!");
+}
+if (a == 6)
+{
+    Console.WriteLine("Sixth day of the week is Saturday!");
+}
+if (a == 7)
+{
+    Console.WriteLine("Seventh day of the week is Sunday!");
+}
