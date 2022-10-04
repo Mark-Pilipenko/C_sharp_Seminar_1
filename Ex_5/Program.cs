91620b8d27923c131247f4999c51a65c6e5e2c5f
Mark <kalmark@mail.ru>
2022-10-04 19:45:42 +0300

    Add Ex_5

---
+++ Ex_5/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Input noun, please:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("All integer nouns from -");
+Console.Write(n);
+Console.Write(" to ");
+Console.Write(n);
+Console.WriteLine(":");
+int i = -n;
+while (i < 0)
+{
+    Console.Write(i);
+    Console.Write(", ");
+    i++;
+}
+i = 0;
+while (i < n)
+{
+    Console.Write(i);
+    Console.Write(", ");
+    i++;
+}
+Console.WriteLine(i);
